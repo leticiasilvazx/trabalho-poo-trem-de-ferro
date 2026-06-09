ba84d4a1ee64bac48d1de8a240c7b6426c74cc52
leticiasilvazx <leticia_2007leo@hotmail.com>
2026-06-09 09:14:18 -0300

    Add embarcar method to VagaoPassageiros class

--- VagaoPassageiros.java
+++ VagaoPassageiros.java
@@ -5,7 +5,17 @@ public class VagaoPassageiros extends Vagao {
     }
 
     @Override
-    public String getTipo() {
+    public boolean embarcar(String item){
+        if(itens.size() < capacidade){
+            itens.add(item);
+            return true;
+        }
+        System.out.println("[Erro] Vagão de passageiros lotado. Não foi possível embarcar: " + item);
+        return false;
+    }
+    
+    @Override
+    public String getTipo(){
         return "Passageiros";
     }
 }
